511a79360859b0ddde23608bf9e0a39e553ef401
safem0de <n.wangwanich@gmail.com>
2023-01-18 16:30:07 +0700

    Add Planning

---
+++ API/Models/Planning.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Models
+{
+    public class Planning : BaseEntity
+    {
+        public string? ShipmentDate { get; set; }
+
+        public int Quantity { get; set; }
+
+        public int OkPart { get; set; }
+
+        public int NgPart { get; set; }
+
+        public Product Product { get; set; }
+
+        public int ProductId { get; set; }
+    }
+}
