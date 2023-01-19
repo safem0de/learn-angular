b231f2cc05d1fe595ff6254f10374a796135ed31
safem0de <n.wangwanich@gmail.com>
2023-01-19 15:04:24 +0700

    lesson 11

---
+++ API/Controllers/CharacterController.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using API.Models;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CharacterController : ControllerBase
+    {
+        private static List<Character> characters = new List<Character>
+        {
+            new Character(),
+            new Character{Name = "Sam"}
+        };
+
+        [HttpGet]
+        public ActionResult<List<Character>> Get()
+        {
+            return Ok(characters);
+        }
+    }
+}
