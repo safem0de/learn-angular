495f90cb65976462ff9e07329299b495cdb97e4c
safem0de <n.wangwanich@gmail.com>
2023-01-24 10:46:10 +0700

    Register and Login

---
+++ API/Controllers/AuthController.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using API.Data;
+using API.DTOs.User;
+using API.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AuthController : ControllerBase
+    {
+        private readonly IAuthRepository _authRepo;
+        public AuthController(IAuthRepository authRepo)
+        {
+            _authRepo = authRepo;
+        }
+
+        [HttpPost("Register")]
+        public async Task<ActionResult<ServiceResponse<int>>> Register (UserRegisterDto request)
+        {
+            var response = await _authRepo.Register(
+                new User { Username = request.Username }, request.Password
+            );
+
+            if(!response.Success){
+                return BadRequest(response);
+            }
+            return Ok(response);
+        }
+
+        [HttpPost("Login")]
+        public async Task<ActionResult<ServiceResponse<int>>> Login (UserLoginDto request)
+        {
+            var response = await _authRepo.Login(request.Username, request.Password);
+
+            if(!response.Success){
+                return BadRequest(response);
+            }
+            return Ok(response);
+        }
+    }
+}
--- API/Data/AuthRepository.cs
+++ API/Data/AuthRepository.cs
@@ -8,19 +8,80 @@ namespace API.Data
 {
     public class AuthRepository : IAuthRepository
     {
-        public Task<ServiceResponse<string>> Login(string username, string password)
+        private readonly DataContext _context;
+        public AuthRepository(DataContext context)
         {
-            throw new NotImplementedException();
+            _context = context;
+        }
+        
+        public async Task<ServiceResponse<string>> Login(string username, string password)
+        {
+            var response = new ServiceResponse<string>();
+            var user = await _context.Users.FirstOrDefaultAsync(u => u.Username.ToLower().Equals(username.ToLower()));
+
+            if(user is null)
+            {
+                response.Success = false;
+                response.Message = "User or Password not correct.";
+            }
+            else if(!VerifyPassword(password, user.PasswordHash, user.PasswordSalt))
+            {
+                response.Success = false;
+                response.Message = "User or Password not correct.";
+            }
+            else
+            {
+                response.Data = user.Id.ToString();
+            }
+
+            return response;
         }
 
-        public Task<ServiceResponse<int>> Register(User user, string password)
+        public async Task<ServiceResponse<int>> Register(User user, string password)
         {
-            throw new NotImplementedException();
+            var response = new ServiceResponse<int>();
+            if(await UserExists(user.Username)){
+                response.Success = false;
+                response.Message = "User already exists.";
+                return response;
+            }
+            CreatePasswordHash(password, out byte[] passwordHash, out byte[] passwordSalt);
+
+            user.PasswordHash = passwordHash;
+            user.PasswordSalt = passwordSalt;
+
+            _context.Users.Add(user);
+            await _context.SaveChangesAsync();
+            
+            response.Data = user.Id;
+            return response;
         }
 
-        public Task<bool> UserExists(string username)
+        public async Task<bool> UserExists(string username)
         {
-            throw new NotImplementedException();
+            if(await _context.Users.AnyAsync(u => u.Username.ToLower() == username.ToLower()))
+            {
+                return true;
+            }
+            return false;
+        }
+
+        private void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
+        {
+            using(var hmac = new System.Security.Cryptography.HMACSHA512())
+            {
+                passwordSalt = hmac.Key;
+                passwordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+            }
+        }
+
+        private bool VerifyPassword(string password, byte[] passwordHash, byte[] passwordSalt)
+        {
+            using(var hmac = new System.Security.Cryptography.HMACSHA512(passwordSalt))
+            {
+                var computedHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+                return computedHash.SequenceEqual(passwordHash);
+            }
         }
     }
 }
